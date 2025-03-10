c4a49bbf178ec2be39f21d9d7a6b708939c634be
Nabieff <vusalnabiyev06@gmail.com>
2025-03-11 00:46:44 +0400

    nv

---
+++ Ucbucaq_sahesi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"Ucbucagin birinci terefi: ";
+	cin>>a;
+	cout<<"Ucbucagin ikinci terefi: ";
+	cin>>b;
+	cout<<"Bu terefler arasindaki bucaq: ";
+	cin>>c;
+	float d;	
+	d=(a*b*c)/2;
+	cout<<"Ucbucagin sahesi: "<<d;
+	return 0;
+	
+	
+}
